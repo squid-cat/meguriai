7e096c3b1aa9ed23d2108f3ead81255683ac62f8
squid-cat <k_imasato@yumemi.co.jp>
2025-07-25 20:24:53 +0900

    fix: 参照先が間違ってたので修正

--- utils/api-client.ts
+++ utils/api-client.ts
@@ -1,5 +1,4 @@
 import { hc } from "hono/client";
-import { AppType } from "../app/api/[...route]";
+import { AppType } from "../app/api/[...route]/route";
 
 export const client = hc<AppType>("http://localhost:3000/").api;
-
