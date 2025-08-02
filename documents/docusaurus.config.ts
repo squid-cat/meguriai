bcb9ef341b509a2899e06286dc42bc09409fcfc9
Taisei-rgb <69156263+Taisei-rgb@users.noreply.github.com>
2025-08-02 22:49:05 +0900

    fix: 細かな修正とルドキュメントの追加

--- documents/docusaurus.config.ts
+++ documents/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'めぐりあい',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -71,19 +71,19 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/docusaurus-social-card.jpg', // TODO: 画像を追加
     navbar: {
-      title: 'My Site',
+      title: 'めぐりあい',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'めぐりあい',
+        src: 'img/logo.svg', // TODO: ロゴ画像を追加
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
